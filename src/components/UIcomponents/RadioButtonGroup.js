4c9655ea664bb436734453b3e91bf0a72317d1f2
anh duong duc <ducanh99tpvpnd@gmail.com>
2022-09-22 19:59:36 +0700

    thêm lý do không chấp thuận

--- src/components/UIcomponents/RadioButtonGroup.js
+++ src/components/UIcomponents/RadioButtonGroup.js
@@ -4,7 +4,16 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 
-export default function RadioButtonsGroup() {
+import BasicTextFields from './TextField';
+
+export default function RadioButtonsGroup(props) {
+    const [formData, setformData] = React.useState();
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setformData(value);
+    };
+
     return (
         <FormControl>
             <RadioGroup
@@ -17,8 +26,18 @@ export default function RadioButtonsGroup() {
                 aria-labelledby="demo-radio-buttons-group-label"
                 name="radio-buttons-group"
             >
-                <FormControlLabel value="chapthuan" control={<Radio />} label="Chấp thuận" />
-                <FormControlLabel value="khongchapthuan" control={<Radio />} label="Không chấp thuận" />
+                <FormControlLabel value="chapthuan" control={<Radio />} label="Chấp thuận" onChange={handleChange} />
+                <FormControlLabel
+                    value="khongchapthuan"
+                    control={<Radio />}
+                    label="Không chấp thuận"
+                    onChange={handleChange}
+                />
+                {formData === 'khongchapthuan' ? (
+                    <div>
+                        <BasicTextFields label="Lý do" />
+                    </div>
+                ) : null}
             </RadioGroup>
         </FormControl>
     );
